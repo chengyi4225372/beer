af4138331632fc56a1ba11d319538918534d5fcf
chengyi <732345907@qq.com>
2019-05-28 16:18:05 +0800

    定制酒

--- Application/Home/Controller/DingController.class.php
+++ Application/Home/Controller/DingController.class.php
@@ -12,7 +12,71 @@ use Home\Controller\BaseController;
 
 class DingController extends  BaseController
 {
+
+    public $table = 'ding';
+    public $cates = 'ding_cates';
+
+
+    public function hot($pid){
+        $arr = M($this->table)->where(array('pid'=>$pid,'is_deleted'=>0))->order('lan desc,id desc')->limit(10)->select();
+        return $arr?$arr:'';
+    }
+
+    //分页
     public function index(){
+        $pid =  I('get.id');
+        $pages= 8;
+        if(empty($pid)){
+            $count =  M($this->table)->where(array('is_deleted'=>0))->count();
+            $size = $count/$pages;
+            $list  = M($this->table)->where(array('is_deleted'=>0))->page($_GET['p'].','.$pages)->select();
+        }else{
+            $count =  M($this->table)->where(array('pid'=>$pid,'is_deleted'=>0))->count();
+            $size = $count/$pages;
+            $list  = M($this->table)->where(array('pid'=>$pid,'is_deleted'=>0))->page($_GET['p'].','.$pages)->select();
+        }
+        $this->assign('size',$size);
+        $this->assign('list',$list);
+        $hot = $this->hot($pid);
+        $this->assign('hot',$hot);
         $this->display();
     }
+
+    public function detail(){
+        $id =I('get.id');
+        $pid = I('get.pid');
+        $info = M($this->table)->where(array('id'=>$id,'pid'=>$pid,'is_deleted'=>0))->find();
+        $this->assign('info',$info);
+        $hot = $this->hot($pid);
+        $this->assign('hot',$hot);
+        $top =$this->top($id);
+        $next = $this->next($id);
+        $this->assign('top',$top);
+        $this->assign('next',$next);
+        $cates = $this->cates($pid);
+        $this->assign('cates',$cates);
+        $this->display();
+    }
+
+
+    //当前所属分类
+    public function cates($pid){
+        $cates = M($this->cates)->field('title')->where(array('id'=>$pid))->find();
+        return $cates;
+    }
+
+
+    //上一篇
+    public function top($id){
+        $top =  M($this->table)->where(array('id'=>array('lt',$id),'is_deleted'=>0))->order(array('id'=>'desc'))->limit(1)->find();
+        return $top?$top:'';
+    }
+
+    //下一篇
+    public function next($id){
+        $next =  M($this->table)->where(array('id'=>array('gt',$id),'is_deleted'=>0))->order('id asc')->limit(1)->find();
+        return $next?$next:'';
+    }
+
+
 }
