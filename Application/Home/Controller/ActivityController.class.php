7d2d932fa6b4930856aa324016cb73f49fd08927
chengyi <732345907@qq.com>
2019-06-01 11:45:28 +0800

    促销活动完成

--- Application/Home/Controller/ActivityController.class.php
+++ Application/Home/Controller/ActivityController.class.php
@@ -15,15 +15,31 @@ class ActivityController extends  BaseController
     public $table ='protuct';
 
     public function index(){
-         $hot =  M($this->table)->where(array('is_show'=>1,'is_deleted'=>0,'is_style'=>3))->order('lan desc')->limit(3)->select();
+         $hot = $this->hot($style=3,$type=1);
+         $phot =$this->hot($style=3,$type=2);
          $yuan = M($this->table)->where(array('is_show'=>1,'is_deleted'=>0,'is_style'=>2))->order('lan desc')->limit(9)->select();
          $style= M($this->table)->where(array('is_show'=>1,'is_deleted'=>0,'is_style'=>1))->order('lan desc')->limit(9)->select();
+         $count =M($this->table)->where(array('is_show'=>1,'is_deleted'=>0,'is_style'=>1))->count();
+         $pages =8;
+         $size = $count/$pages;
+         $pstyle= M($this->table)->where(array('is_show'=>1,'is_deleted'=>0,'is_style'=>1))->order('lan desc')->page($_GET['p'].','.$pages)->select();
          $this->assign('hot',$hot);
          $this->assign('yuan',$yuan);
          $this->assign('style',$style);
+         $this->assign('phot',$phot);
+         $this->assign('size',$size);
+         $this->assign('pstyle',$pstyle);
          $this->display();
     }
 
-
+    //1 PC  ·´Ö® phone
+    public function hot($style=3,$type=1 ){
+        if($type==1){
+            $arr = M($this->table)->where(array('is_show'=>1,'is_deleted'=>0,'is_style'=>$style))->order('lan desc')->limit(3)->select();
+        }else{
+            $arr = M($this->table)->where(array('is_show'=>1,'is_deleted'=>0,'is_style'=>$style))->order('lan desc')->limit(6)->select();
+        }
+        return $arr ? $arr:'';
+    }
 
 }
