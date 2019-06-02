ffd95f154b32ab5995d9123a72da4939953dfb2d
chengyi <cheng@163.com>
2019-06-02 22:51:45 +0800

    视频完成 项目完成

--- Application/Admin/Controller/BaseController.class.php
+++ Application/Admin/Controller/BaseController.class.php
@@ -43,23 +43,33 @@ class BaseController extends Controller
 
         // 上传文件 
         $info   =   $upload->upload();
-
         if(!$info) {// 上传错误提示错误信息
-
             $this->error($upload->getError());
-
         }else{// 上传成功
-
             foreach($info as $file){
-
                 echo $file['savepath'].$file['savename'];
-
             }
-
         }
-
     }
 
+    public function uploadvideo()
+    {
+        $filenames = $_FILES['file']['name'];
+        $upload = new \Think\Upload();// 实例化上传类
+        $upload->maxSize   =     999999999 ;// 设置附件上传大小
+        $upload->exts      =     array('mp4', 'avi', 'webm');// 设置附件上传类型
+        $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+        $upload->savePath  =     ''; // 设置附件上传（子）目录
+        // 上传文件
+        $info   =   $upload->upload();
+        if(!$info) {// 上传错误提示错误信息
+            $this->error($upload->getError());
+        }else{// 上传成功
+            foreach($info as $file){
+                echo $file['savepath'].$file['savename'];
+            }
+        }
+    }
 
     /**
      * KindEditor 编辑器
